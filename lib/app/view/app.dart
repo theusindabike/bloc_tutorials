5e88e6b2862f74a41243c1613158b0bc1d47e04a
Matheus Lopes <mlopes.matheus@gmail.com>
2022-12-13 23:38:54 -0300

    adding auth bloc

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,29 +1,77 @@
-import 'package:bloc_tutorials/posts/view/posts_page.dart';
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/authentication/authentication.dart';
+import 'package:flutter_login/home/home.dart';
+import 'package:flutter_login/login/login.dart';
+import 'package:flutter_login/splash/splash.dart';
+import 'package:user_repository/user_repository.dart';
 
-class App extends MaterialApp {
-  const App({super.key}) : super(home: const PostsPage());
+class App extends StatelessWidget {
+  const App({
+    super.key,
+    required this.authenticationRepository,
+    required this.userRepository,
+  });
+
+  final AuthenticationRepository authenticationRepository;
+  final UserRepository userRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+          userRepository: userRepository,
+        ),
+        child: const AppView(),
+      ),
+    );
+  }
 }
-// import 'package:flutter/material.dart';
-// import 'package:bloc_tutorials/counter/counter.dart';
-// import 'package:bloc_tutorials/l10n/l10n.dart';
 
-// class App extends StatelessWidget {
-//   const App({super.key});
+class AppView extends StatefulWidget {
+  const AppView({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData(
-//         appBarTheme: const AppBarTheme(
-//                  color: Color.fromARGB(255, 26, 2, 82)),
-//         colorScheme: ColorScheme.fromSwatch(
-//           accentColor: Color.fromARGB(255, 212, 255, 19),
-//         ),
-//       ),
-//       localizationsDelegates: AppLocalizations.localizationsDelegates,
-//       supportedLocales: AppLocalizations.supportedLocales,
-//       home: const CounterPage(),
-//     );
-//   }
-// }
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState!;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  HomePage.route(),
+                  (route) => false,
+                );
+                break;
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+                break;
+              case AuthenticationStatus.unknown:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+      onGenerateRoute: (_) => SplashPage.route(),
+    );
+  }
+}
--- lib/authentication/bloc/authentication_event.dart
+++ lib/authentication/bloc/authentication_event.dart
@@ -2,7 +2,20 @@ part of 'authentication_bloc.dart';
 
 abstract class AuthenticationEvent extends Equatable {
   const AuthenticationEvent();
+}
+
+class _AuthenticationStatusChanged extends AuthenticationEvent {
+  const _AuthenticationStatusChanged(this.status);
+
+  final AuthenticationStatus status;
 
   @override
-  List<Object> get props => [];
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class AuthenticationLogoutRequested extends AuthenticationEvent {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
 }
