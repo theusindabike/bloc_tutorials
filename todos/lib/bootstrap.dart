50bff17e5a85f2bc0128aea8a66fe8de46ceceb5
Matheus Lopes <mlopes.matheus@gmail.com>
2023-02-13 16:24:15 -0300

    feat:tutorial part 2

--- todos/lib/bootstrap.dart
+++ todos/lib/bootstrap.dart
@@ -3,32 +3,22 @@ import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
+import 'package:todos/app/app.dart';
+import 'package:todos/app/app_bloc_observer.dart';
+import 'package:todos_api/todos_api.dart';
+import 'package:todos_repository/todos_repository.dart';
 
-class AppBlocObserver extends BlocObserver {
-  const AppBlocObserver();
-
-  @override
-  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
-    super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
-  }
-
-  @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
-    super.onError(bloc, error, stackTrace);
-  }
-}
-
-Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+void bootstrap({required TodosApi todosApi}) {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
   Bloc.observer = const AppBlocObserver();
 
-  await runZonedGuarded(
-    () async => runApp(await builder()),
+  final todosRepository = TodosRepository(todosApi: todosApi);
+
+  runZonedGuarded(
+    () => runApp(App(todosRepository: todosRepository)),
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
--- todos/test/helpers/helpers.dart
+++ todos/test/helpers/helpers.dart
@@ -1,1 +1,3 @@
+export 'finders.dart';
+export 'l10n.dart';
 export 'pump_app.dart';
